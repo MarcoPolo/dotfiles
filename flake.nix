5a55b78f1ddeb4f1c344f38929ab3d0319dfc524
Marco Munizaga <git@marcopolo.io>
2023-02-11 00:38:42 +0000

    Add home-manager

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.home-manager.url = "github:nix-community/home-manager/release-22.11";
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { system = system; };
+      in
+      {
+        packages.home-manager = inputs.home-manager.${system};
+        defaultPackage = self.packages.${system}.hello;
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.hello ];
+        };
+        # nix build .#homeManagerConfigurations.x86_64-linux.codespace.activationPackage && result/activate
+        # nix run .#home-manager -- switch --flake ".#codespace"
+        packages.homeConfigurations.codespace = inputs.home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [
+              ./home-manager/home.nix
+
+            ];
+          };
+      });
+}
--- install.sh
+++ install.sh
@@ -6,3 +6,17 @@ sh <(curl -L https://nixos.org/nix/install)
 
 sudo mkdir -p /etc/nix
 sudo cp nix.conf /etc/nix/nix.conf
+
+sudo apt update -y
+sudo apt -y install --no-install-recommends acl
+
+# github codespaces fixes
+if [ "${CODESPACES}" = true ]; then
+  # vscode codespaces set default permissions on /tmp. These will
+  # produce invalid permissions on files built with nix. This fix
+  # removes default permissions set on /tmp
+  echo "fixing perms"
+  sudo setfacl --remove-default /tmp
+fi
+
+nix run .#home-manager -- switch --flake ".#codespace" -b backup
